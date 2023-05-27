4f8ff7e6c0a46211ff4ed1fe9585f9e3404b2f72
Ashwin Gadve <114279268+Ashwing43@users.noreply.github.com>
2023-05-27 15:10:55 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-  cout<<"HELLO WORLD!!!!!"<<endl;
+  int a;
+  cout<<"Program to check if \n you are eligible to vote:"<<endl;
+  cout<<"enter your age"<<endl;
+  cin>>a;
+  if(a>=18)
+      cout<<"Yes\n";
+  else
+      cout<<"No\n";
   return 0;
 }
